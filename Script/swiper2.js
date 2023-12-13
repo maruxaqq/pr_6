b755deb95fb70b400c66f1c4df5f67fa783aa17d
maruxaqq <144634783+maruxaqq@users.noreply.github.com>
2023-12-14 08:38:06 +0900

    Add files via upload

---
+++ Script/swiper2.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const resizableSwiper = (breakpoint, swiperClass, swiperSettings, callback) => {
+      let swiper;
+  
+      breakpoint = window.matchMedia(breakpoint);
+  
+      const enableSwiper = function(className, settings) {
+        swiper = new Swiper(className, settings);
+  
+        if (callback) {
+          callback(swiper);
+        }
+      }
+  
+      const checker = function() {
+        if (breakpoint.matches) {
+          return enableSwiper(swiperClass, swiperSettings);
+        } else {
+          if (swiper !== undefined) swiper.destroy(true, true);
+          return;
+        }
+      };
+  
+      breakpoint.addEventListener('change', checker);
+      checker();
+    }
+  
+    const someFunc = (instance) => {
+      if (instance) {
+        instance.on('slideChange', function (e) {
+          console.log('*** mySwiper.activeIndex', instance.activeIndex);
+        });
+      }
+    };
+  
+    resizableSwiper(
+      '(max-width: 426px)',
+      '.slider-2',
+      {
+        loop: true,
+        spaceBetween: 32,
+        slidesPerView: 1,
+        pagination: {
+          el: '.swiper-pagination-2',
+          clickable: true,
+        },
+      },
+      someFunc
+    );
+    });
+
+
